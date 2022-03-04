34ba5212a783235976c6f83ec22d4435e98b9044
xebec19 <rohant946@gmail.com>
2022-03-04 23:18:26 +0530

    save

--- structures/main.rs
+++ structures/main.rs
@@ -1,17 +1,36 @@
-struct Rectangle {
+#[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: dbg!(32),
+//         height: 32
+//     };
+//     println!(
+//         "The area of rectangle is {:#?}",
+//         rect1
+//     );
+// }
+struct Reactangle {
     width: u32,
     height: u32,
 }
+
+impl Reactangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    let rect1 = Rectangle {
+    let react1 = Reactangle{
         width: 30,
-        height: 50
+        height: 50,
     };
     println!(
-        "The area of rectangle is {}",
-        area(&rect1)
+        "The area of the reactangle is {} square pixels.",
+        react1.area()
     );
-}
-fn area(rectangle: &Rectangle) -> u32 {
-    reactangle.width * rectangle.height
 }
