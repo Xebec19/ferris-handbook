731bbf7d4e11c1148d7865b6b48da9252d9eb69f
xebec19 <rohant946@gmail.com>
2022-03-06 15:35:03 +0530

    save

--- growing-projects/restaurant/src/lib.rs
+++ growing-projects/restaurant/src/lib.rs
@@ -1,8 +1,42 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+        fn seat_at_table() {}
     }
+    use self::front_of_house::hosting;
+    pub fn eat_at_restaurant() {
+        hosting::add_to_waitlist();
+        hosting::add_to_waitlist();
+        hosting::add_to_waitlist();
+        // Absolute path
+        crate::front_of_house::hosting::add_to_waitlist();
+        // Relative path
+        front_of_house::hosting::add_to_waitlist();
+    }
+    mod serving {
+        fn take_order() {}
+        fn serve_order() {}
+        fn take_payment() {}
+    }
+}
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruits: String
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast{
+                toast: String::from(toast),
+                seasonal_fruits: String::from("peaches")
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("wheat");
+    println!("I'd like {} toast please",meal.toast);
 }
